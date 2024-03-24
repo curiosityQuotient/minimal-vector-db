89113f8ff5440af987f5fdfac88e8b30b39cd25a
CuriosityQuotient <CuriosityQuotient>
2024-03-24 21:58:06 +0000

    feat: Added TextChunker class with chunk_text method

--- min-vec-db.py
+++ min-vec-db.py
@@ -8,13 +8,48 @@ import pandas as pd
 from pypdf import PdfReader
 
 
-class documentLoader:
+class DocumentLoader:
     """
     A class for loading documents to text and chunking them.
     """
 
     def load_pdf(self, filepath: str):
-
+        """method for loading text from pdf into object"""
         # load pdf
+        reader = PdfReader(filepath)
         text = ""
+        for page in reader.pages:
+            text = text + page.extract_text()
         self.text = text
+
+
+class TextChunker:
+    """
+    A class for chunking text that has been loaded
+    """
+
+    def chunk_text(self, text: str, chunkLen: int, overlap: int):
+        # add chunking function
+        chunks = []
+        chunkEnd = text.find(" ")
+        while chunkEnd >= 0:
+            chunkEnd = text.find(" ")
+            nn = 0
+            while nn < chunkLen and chunkEnd >= 0:
+                chunkEnd = text.find(" ", chunkEnd + 1)
+                nn += 1
+            chunks.append(text[:chunkEnd])
+            mm = 0
+            nextChunk = chunkEnd
+            while mm < overlap:
+                nextChunk = text.rfind(" ", 0, nextChunk)
+                mm += 1
+            text = text[nextChunk:]
+        self.chunks = chunks
+
+
+docLoader = DocumentLoader()
+docLoader.load_pdf("Everyone-loves-trees.pdf")
+chunker = TextChunker()
+chunker.chunk_text(docLoader.text, 25, 3)
+print("lol")
